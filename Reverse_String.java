94cc45e3eff9733b7eae18580743eb89a285e61e
sriram <97436480+ram1494@users.noreply.github.com>
2022-10-14 18:47:20 +0530

    import java.util.*; class solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         String n="";         for(int i=s.length()-1;i>=0;i--)         {             n+=s.charAt(i);         }         System.out.println(n);     } }

---
+++ Reverse_String.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String n="";
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            n+=s.charAt(i);
+        }
+        System.out.println(n);
+    }
+}

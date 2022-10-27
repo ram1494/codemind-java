646dad2dd6f85b8baf84cf85369acef8742b0470
sriram <97436480+ram1494@users.noreply.github.com>
2022-10-27 10:47:50 +0530

    import java.util.*; class crc {     public static void main(String args[])     {         Scanner sc=new Scanner(System .in);         float r;         r=sc.nextFloat();         double ar=3.14*r*r;         System.out.format("%.2f",ar);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class crc
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System .in);
+        float r;
+        r=sc.nextFloat();
+        double ar=3.14*r*r;
+        System.out.format("%.2f",ar);
+    }
+}

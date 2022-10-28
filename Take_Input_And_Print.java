1bf08410e6efc476ecd958d8c9aa870b38e000ee
sriram <97436480+ram1494@users.noreply.github.com>
2022-10-28 10:28:26 +0530

    import java.util.*; class Input {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String si=sc.nextLine();         System.out.println(si);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Input
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String si=sc.nextLine();
+        System.out.println(si);
+    }
+}

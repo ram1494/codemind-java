d82b2035b9fc6aaddbfeea6bd0bd7651957d08d1
sriram <97436480+ram1494@users.noreply.github.com>
2022-10-29 11:01:04 +0530

    import java.util.*; class picha {     public static void main(String  args[])     {         Scanner sc=new Scanner(System.in);         int n,h,hs;         n=sc.nextInt();         h=n-1;         hs=(h*n)/2;         System.out.println(hs);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class picha
+{
+    public static void main(String  args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,h,hs;
+        n=sc.nextInt();
+        h=n-1;
+        hs=(h*n)/2;
+        System.out.println(hs);
+    }
+}
